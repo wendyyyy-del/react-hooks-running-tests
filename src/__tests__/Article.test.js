f19f79eb37377817dea4e2c7402efd13a047ad04
sarah <sarah.akelo@student.moringaschool.com>
2025-04-08 12:36:36 +0300

    first lab complete

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -7,5 +7,8 @@ import Article from "../components/Article";
 test("displays the text 'please pass this test'", () => {
   render(<Article />);
 
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  
+  screen.debug();
+
+  expect(screen.queryByText("please pass this test")).toBeInTheDocument()
 });
